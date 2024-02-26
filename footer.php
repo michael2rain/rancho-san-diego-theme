e353daae467ece535fc50503570e44adda5c3284
m2rain <debil_k2@hotmail.com>
2024-02-25 23:21:42 -0600

    Commit inicial

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Hotel_Rancho_San_Diego
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<p>Tema desarrollado por <a target="_blank" href="https://semantica.mx/">Semántica</a></p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
