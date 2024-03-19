49b2dfbe11496c0a7b741b790494a11463f9fe4a
m2rain <debil_k2@hotmail.com>
2024-03-18 20:54:22 -0600

    Agregado: Enlaces para reservar

--- js/navigation.js
+++ js/navigation.js
@@ -33,3 +33,46 @@ document.addEventListener('DOMContentLoaded', () => {
 		window.addEventListener('scroll', () => stickyHeader(getHeader))
 	);
 });
+
+
+/**
+ * Asignar enlaces a las tarjetas de habitaciones y al botón de "Reservar ahora".
+ */
+const addLinks = () => {
+	const now = new Date();
+	const checkInDate = now.toISOString().split("T")[0];
+	const checkOutDate = new Date(now.setDate(now.getDate() + 1)).toISOString().split("T")[0];
+	const localeData = {
+		'en-US': {
+			currency: 'USD',
+			lang: 'en'
+		},
+		'es-MX': {
+			currency: 'MXN',
+			lang: 'es'
+		}
+	};
+
+	let locale;
+	if (localeData[document.documentElement.lang]) {
+		locale = localeData[document.documentElement.lang];
+	}
+
+	const domainBase = "https://direct-book.com/properties/HotelRanchoSanDiegoDIRECT";
+	const params = `?locale=${locale.lang}&items[0][adults]=2&items[0][children]=0&items[0][infants]=0&currency=${locale.currency}&checkInDate=${checkInDate}&checkOutDate=${checkOutDate}&trackPage=yes`;
+
+	const rooms = document.querySelectorAll('.item__card');
+	if (rooms) {
+		rooms.forEach((room) => {
+			room.setAttribute('href', `${domainBase}${params}`);
+		});
+	}
+
+	const bookNowButton = document.getElementById('bookNowButton');
+	if (bookNowButton) {
+		const bookNowLink = bookNowButton.querySelector('a');
+		bookNowLink.setAttribute('href', `${domainBase}${params}`);
+	}
+
+};
+document.addEventListener('DOMContentLoaded', addLinks)
