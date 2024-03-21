2487c5ce8af0e489974ad971314e84255f9c3403
m2rain <debil_k2@hotmail.com>
2024-03-21 17:54:21 -0600

    Style: Menu modal, bottones flotantes.

--- js/navigation.js
+++ js/navigation.js
@@ -12,7 +12,6 @@ const navDataHover = () => {
 		link.setAttribute('data-hover', link.textContent);
 	}
 };
-document.addEventListener('DOMContentLoaded', navDataHover)
 
 /**
  * Intercambiar clases para efecto de sticky header.
@@ -21,19 +20,23 @@ const stickyHeader = (header) => {
 	if (window.scrollY > 150) {
 		header.classList.add("stickyOn");
 		header.classList.remove("stickyOff");
-	} else {
+	} else if (window.scrollY <= 150) {
+		const modalStatus = document.querySelector('body').getAttribute('data-modal-open');
+		if (modalStatus === 'true') {
+			return;
+		};
 		header.classList.add("stickyOff");
 		header.classList.remove("stickyOn");
 	}
 }
-document.addEventListener('DOMContentLoaded', () => {
+
+const initStickyHeader = () => {
 	const getHeader = document.getElementById('hrsdHeader');
 	!getHeader ? null : (
 		stickyHeader(getHeader),
 		window.addEventListener('scroll', () => stickyHeader(getHeader))
 	);
-});
-
+};
 
 /**
  * Asignar enlaces a las tarjetas de habitaciones y al botón de "Reservar ahora".
@@ -75,4 +78,95 @@ const addLinks = () => {
 	}
 
 };
-document.addEventListener('DOMContentLoaded', addLinks)
+
+/**
+ * Mostrar y ocultar botones flotantes
+ */
+let excuted = false;
+const toggleClass = (element) => {
+	if (window.scrollY >= window.innerHeight / 3) {
+		excuted = true;
+		element.classList.add("show-buttons");
+		element.classList.remove("hide-buttons");
+
+	} else {
+		if (excuted) {
+			element.classList.add("hide-buttons");
+			element.classList.remove("show-buttons");
+		}
+	}
+};
+
+const scrollButtons = () => {
+	let documentHeight = document.documentElement.scrollHeight;
+	documentHeight >= 6500 ? documentHeight = 6500 : documentHeight = documentHeight;
+
+	// 5s a que el iframe se cargue
+	let chatBubble;
+	setTimeout(() => {
+		chatBubble = document.getElementById('messengeriframe');
+		window.addEventListener('scroll', () => {
+			toggleClass(chatBubble);
+		});
+	}, 3000);
+
+	const floatContainer = document.querySelector('.float-buttons');
+	floatContainer.setAttribute('style', `--document-height: ${documentHeight}px;`);
+
+	window.addEventListener('scroll', () => {
+		toggleClass(floatContainer);
+	});
+
+	const body = document.querySelector('body');
+	const menuButton = document.querySelector('div[aria-label="Menu Toggle"]');
+	const bookNowButton = document.getElementById('bookNowButton');
+	let menuOpen = false;
+	let menuExecuted = false;
+	menuButton.addEventListener('click', () => {
+		if (menuOpen) {
+			menuExecuted = true;
+			menuOpen = false;
+
+			toggleClass(floatContainer);
+			toggleClass(chatBubble);
+
+			bookNowButton.classList.remove("book-now-modal");
+
+			if (body.style.position === 'fixed') {
+				const scrollY = document.body.style.top;
+				document.body.style.position = '';
+				document.body.style.top = '';
+				window.scrollTo(0, parseInt(scrollY || '0') * -1);
+				body.setAttribute('data-modal-open', 'false');
+			}
+
+		} else {
+			menuOpen = true;
+			bookNowButton.classList.add("book-now-modal");
+			if (menuExecuted || window.scrollY >= window.innerHeight / 3) {
+				floatContainer.classList.remove("show-buttons");
+				floatContainer.classList.add("hide-buttons");
+
+				if (chatBubble) {
+					chatBubble.classList.remove("show-buttons");
+					chatBubble.classList.add("hide-buttons");
+				}
+			}
+
+			const statusScrollY = window.scrollY;
+			body.style.position = 'fixed';
+			body.style.top = `-${statusScrollY}px`;
+			body.setAttribute('data-modal-open', 'true');
+		}
+	});
+};
+
+/**
+ * Inicializar funciones al cargar el DOM.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+	navDataHover();
+	initStickyHeader();
+	addLinks();
+	scrollButtons();
+});
